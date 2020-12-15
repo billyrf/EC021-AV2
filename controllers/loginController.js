7eb6b38e8507248ecafbc41231d5e75e09dfbdba
Billy Rodrigues <billym@gec.inatel.br>
2020-12-15 10:17:48 -0300

    Validando o body

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,20 +2,30 @@ require('dotenv').config();
 const axios = require('axios');
 
 module.exports = {
-    login(req, res) {
+    login(req, res, next) {
         const { body } = req;
-        const { username, password } = body;
-    
-        axios.post('https://ec021-av2-auth.herokuapp.com/auth/login', {
+
+        function isNotNullUndefined(obj) {
+            return obj !== null && obj !== undefined;
+        }
+
+        if (isNotNullUndefined(body)) {
+
+            const { username, password } = body
+
+            axios.post('https://ec021-av2-auth.herokuapp.com/auth/login', {
             username,
             password
-        })
-            .then((response) => {
+
+        }).then((response) => {
                 return res.json(response.status, response.data);
-            })
-            .catch((error) => {
+            }).catch((error) => {
                 return res.json(error.response.status, error.response.data);
             });
+        } else {
+            res.send(403, 'Favor fornecer o body "username" : "<username>" e "password" : "<password>"');
+        }
+        
     }
 
 }
